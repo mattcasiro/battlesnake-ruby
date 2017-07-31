87e29ef3ae695a33fb271e4b380aae79035d2bc5
Matthew Casiro <mattcasiro@gmail.com>
2017-07-30 22:27:18 -0700

    Get basic functionality working

--- web.rb
+++ web.rb
@@ -2,12 +2,7 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-    responseObject = {
-        "color"=> "#fff000",
-        "head_url"=> "url/to/your/img/file"
-    }
-
-    return responseObject.to_json
+    "Why are you here scrub? Don't you know how to API?"
 end
 
 post '/start' do
@@ -15,13 +10,19 @@ post '/start' do
     requestJson = requestBody ? JSON.parse(requestBody) : {}
 
     # Get ready to start a game with the request data
+    # eg: {"width"=>20, "height"=>20, "game_id"=>"8b262835-cd6e-416b-b667-a162c96a612f"}
 
-    # Dummy response
     responseObject = {
-        "taunt" => "battlesnake-ruby",
+        "name" => "skinny-snek",
+        "color" => "#8107C4",
+        "secondary_color" => "AB8D00",
+        "head_type" => "bendr",
+        "tail_type" => "freckled",
+        "head_url"=> "http://i604.photobucket.com/albums/tt127/ZulackiDrood/Private/Log.Horizon.Avatar.jpg",
+        "taunt" => "Do you SMELL. What the SNEK. IS COOKING?!",
     }
 
-    return responseObject.to_json
+    responseObject.to_json
 end
 
 post '/move' do
@@ -32,11 +33,11 @@ post '/move' do
 
     # Dummy response
     responseObject = {
-        "move" => "north", # One of either "north", "east", "south", or "west".
+        "move" => "up",
         "taunt" => "going north!",
     }
 
-    return responseObject.to_json
+    responseObject.to_json
 end
 
 post '/end' do
@@ -46,5 +47,5 @@ post '/end' do
     # No response required
     responseObject = {}
 
-    return responseObject.to_json
+    responseObject.to_json
 end
